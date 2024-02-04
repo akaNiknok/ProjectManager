6bd8743d2075b13cceb3c292f22a5959ae956e42
akaNiknok <austingimperial@gmail.com>
2024-02-05 03:46:26 +0800

    Initial migrate and add todos to models

--- ProjectManager/PMApp/models.py
+++ ProjectManager/PMApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    # TODO
+    pass
+
+
+class Task(models.Model):
+    # TODO
+    pass
+
+
+class Expense(models.Model):
+    # TODO
+    pass
+
+
+class User(models.Model):
+    # TODO
+    pass
+
+
+class Member(models.Model):
+    # TODO
+    pass
+
+
+class IndividualTaskAssignment(models.Model):
+    # TODO
+    pass
