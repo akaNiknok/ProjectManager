cdc27c4b7241bb2dc7eaa2aff0c1bbd6b874364e
akaNiknok <austingimperial@gmail.com>
2024-02-05 05:05:06 +0800

    Create models

--- ProjectManager/PMApp/models.py
+++ ProjectManager/PMApp/models.py
@@ -2,30 +2,48 @@ from django.db import models
 
 
 class Project(models.Model):
-    # TODO
-    pass
-
-
-class Task(models.Model):
-    # TODO
-    pass
-
-
-class Expense(models.Model):
-    # TODO
-    pass
+    project_id = models.AutoField(primary_key=True)
+    project_name = models.CharField(max_lenth=255)
+    project_desc = models.TextField()
+    project_start = models.DateField()
+    project_end = models.DateField()
+    project_status  # TODO: Enum
 
 
 class User(models.Model):
-    # TODO
-    pass
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_lenth=255)
+    username = models.CharField(max_lenth=255)
+    password = models.CharField(max_lenth=255)  # TODO: password encryption
+    profile_pic  # TODO: profile pics
+    staff_type   # TODO: staff type
 
 
 class Member(models.Model):
-    # TODO
-    pass
+    member_id = models.AutoField(primary_key=True)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    task_id = models.AutoField(primary_key=True)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    task_name = models.CharField(max_lenth=255)
+    task_notes = models.TextField()
+    task_status  # TODO: task status
+    task_priority  # TODO: task status
 
 
 class IndividualTaskAssignment(models.Model):
-    # TODO
-    pass
+    task_assignment_id = models.AutoField(primary_key=True)
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
+    member_id = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+
+class Expense(models.Model):
+    expense_id = models.AutoField(primary_key=True)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    member_id = models.ForeignKey(Member, on_delete=models.CASCADE)
+    expense_date = models.DateField()
+    expense_description = models.TextField()
+    expense_amount = models.DecimalField()  # TODO: Change in data dictionary
