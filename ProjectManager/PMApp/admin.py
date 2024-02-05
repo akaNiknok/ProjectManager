24c1eb1bc2ee7766efbdfc5662030c881847d14c
akaNiknok <austingimperial@gmail.com>
2024-02-06 00:27:14 +0800

    Initialize admin site and temporary test data

--- ProjectManager/PMApp/admin.py
+++ ProjectManager/PMApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, User, Member, Task, IndividualTaskAssignment, Expense
+
+admin.site.register(Project)
+admin.site.register(User)
+admin.site.register(Member)
+admin.site.register(Task)
+admin.site.register(IndividualTaskAssignment)
+admin.site.register(Expense)
