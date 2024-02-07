723335d77be42178a54b67b61ad51f7b8e4ac09d
akaNiknok <austingimperial@gmail.com>
2024-02-07 20:31:00 +0800

    Update urls.py and create view.py placeholders

---
+++ ProjectManager/PMApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("view_project", views.view_project, name="view_project"),
+    path("create_project", views.create_project, name="create_project"),
+    path("update_project", views.update_project, name="update_project"),
+    path("delete_project", views.delete_project, name="delete_project"),
+    path("view_task", views.view_task, name="view_task"),
+    path("create_task", views.create_task, name="create_task"),
+    path("update_task", views.update_task, name="update_task"),
+    path("delete_task", views.delete_task, name="delete_task")
+]
--- ProjectManager/PMApp/views.py
+++ ProjectManager/PMApp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    # TODO: Index
+    return HttpResponse("Index")
+
+def view_project(request):
+    # TODO: View project
+    return HttpResponse("View Project")
+
+def create_project(request):
+    # TODO: Create project 
+    return HttpResponse("Create Project")
+
+def update_project(request):
+    # TODO: Update project 
+    return HttpResponse("Create Project")
+
+def delete_project(request):
+    # TODO: Delete project
+    return HttpResponse("Delete Project")
+
+def view_task(request):
+    # TODO: View task
+    return HttpResponse("View Task")
+
+def create_task(request):
+    # TODO: Create task 
+    return HttpResponse("Create Task")
+
+def update_task(request):
+    # TODO: Update task 
+    return HttpResponse("Create Task")
+
+def delete_task(request):
+    # TODO: Delete task
+    return HttpResponse("Delete Task")
