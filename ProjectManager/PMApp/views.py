fdf6b6bcab9ecbbf6754779da2e773add3e72934
akaNiknok <austingimperial@gmail.com>
2024-02-10 22:31:08 +0800

    Add update_project view

--- ProjectManager/PMApp/views.py
+++ ProjectManager/PMApp/views.py
@@ -46,9 +46,43 @@ def view_project(request, project_id):
                   "view_project.html",
                   {"project": project_obj})
 
-def update_project(request):
-    # TODO: Update project 
-    return HttpResponse("Create Project")
+
+def update_project(request, project_id):
+
+    if (request.method == "POST"):
+
+        project_name = request.POST.get("project_name")
+        project_desc = request.POST.get("project_desc")
+        project_start = request.POST.get("project_start")
+        project_end = request.POST.get("project_end")
+        project_status = request.POST.get("project_status")
+
+        if project_end == "":
+            project_end = None
+
+        project_obj = Project.objects.get(project_id=project_id)
+
+        project_obj.project_name = project_name
+        project_obj.project_desc = project_desc
+        project_obj.project_start = project_start
+        project_obj.project_end = project_end
+        project_obj.project_status = project_status
+
+        project_obj.save()
+
+        return redirect("view_project", project_id=project_obj.project_id)
+
+    else:
+
+        try:
+            project_obj = Project.objects.get(project_id=project_id)
+        except:
+            raise Http404("Project does not exist")
+
+        return render(request,
+                    "update_project.html",
+                    {"project": project_obj})
+
 
 def delete_project(request):
     # TODO: Delete project
