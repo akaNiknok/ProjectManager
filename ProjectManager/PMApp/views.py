e3748a19adb8f4e51c50d6d45e406f55029e587f
akaNiknok <austingimperial@gmail.com>
2024-02-10 21:44:59 +0800

    Add create_project view.

--- ProjectManager/PMApp/views.py
+++ ProjectManager/PMApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, Http404
+from django.shortcuts import render, redirect, Http404
 from django.http import HttpResponse
 
 from .models import Project, User, Member, Task, IndividualTaskAssignment, Expense
@@ -7,6 +7,34 @@ from .models import Project, User, Member, Task, IndividualTaskAssignment, Expen
 def index(request):
     # TODO: Index
     return HttpResponse("Index")
+
+
+def create_project(request):
+
+    if (request.method == "POST"):
+        project_name = request.POST.get("project_name")
+        project_desc = request.POST.get("project_desc")
+        project_start = request.POST.get("project_start")
+        project_end = request.POST.get("project_end")
+        project_status = request.POST.get("project_status")
+
+        if project_end == "":
+            project_end = None
+
+        new_project = Project.objects.create(
+            project_name=project_name,
+            project_desc=project_desc,
+            project_start=project_start,
+            project_end=project_end,
+            project_status=project_status
+        )
+
+        return redirect("view_project", project_id=new_project.project_id)
+
+    else:
+        return render(request,
+                    "create_project.html")
+
 
 def view_project(request, project_id):
     try:
@@ -17,10 +45,6 @@ def view_project(request, project_id):
     return render(request,
                   "view_project.html",
                   {"project": project_obj})
-
-def create_project(request):
-    # TODO: Create project 
-    return HttpResponse("Create Project")
 
 def update_project(request):
     # TODO: Update project 
