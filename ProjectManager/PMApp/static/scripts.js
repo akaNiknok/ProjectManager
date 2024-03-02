ac4afb332452cbc1713cebdb72efb2b8a9b67705
akaNiknok <austingimperial@gmail.com>
2024-03-02 20:53:29 +0800

    Add initial hiding of tasks/expenses

---
+++ ProjectManager/PMApp/static/scripts.js
@@ -0,0 +1,28 @@
+// Script to automatically redirect when selected new project using projct selector
+document.querySelectorAll("[name=project_selector]")[0].addEventListener('change',
+function () {
+    window.location = "{% url 'switch_project' %}" + this.value;
+});
+
+// Hides the div of either tasks or expenses
+// and shows the appropriate form
+function showNewForm(type) {
+
+    // Get the inverse of type and store it to hideType
+    if (type === "tasks") {
+        var hideType = "expenses"
+    } else {
+        var hideType = "tasks"
+    }
+
+    // Hide the div
+    var divToHide = document.getElementById(hideType);
+    if (divToHide.style.display === "none") {
+        // TODO: Might need to change based on display set by Bootstrap
+        divToHide.style.display = "block";  
+    } else {
+        divToHide.style.display = "none";
+    };
+
+    // Show the form
+}
